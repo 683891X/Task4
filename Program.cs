42a76eeb38e4e48d991ea4a3c8f0a35decc64dc6
683891X <683891X@student.swin.edu.au>
2021-03-19 17:32:32 +1100

    Exercise two task one

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String username = "Magic";
+            String password = "Number";
+            String usernameInput;
+            String passwordInput;
+
+            Console.WriteLine("Please enter your username: ");
+            usernameInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password: ");
+            passwordInput = Console.ReadLine();
+
+            if(usernameInput == username){
+                if(passwordInput == password){
+                    Console.WriteLine("Login successful!");
+                }else{
+                    Console.WriteLine("Login unsuccessful");
+                }
+            }else{
+                Console.WriteLine("Login unsuccessful");
+            }
+        }
+    }
+}
